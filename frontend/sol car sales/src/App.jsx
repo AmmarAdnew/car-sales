13c3b7b61642c3346870ac1e7ccb521466cf6d60
AmmarAdnew <132451928+AmmarAdnew@users.noreply.github.com>
2024-10-03 16:16:09 +0000

    Started

---
+++ frontend/sol car sales/src/App.jsx
@@ -0,0 +1,17 @@
+import Navheader from './components/Navheader';
+import HeroSection from './components/HeroSection';
+import SecFooter from './components/SecFooter';
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <Navheader />
+      <HeroSection />
+      <SecFooter />
+    </div>
+  );
+}
+
+export default App
